050bde205c3a9ea45ae589fb1256e74f6733c666
SeongbumSeo <sBum.Seo@gmail.com>
2017-09-22 01:07:05 +0900

    normalize.css

--- views/main.php
+++ views/main.php
@@ -2,10 +2,12 @@
 <html lang="ko">
 <head>
     <meta charset="utf-8" />
-    <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
-    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible"/>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible" />
 
     <title>국가암정보센터</title>
+
+    <link rel="stylesheet" href="../stylesheets/normalize.css" />
 </head>
 
 <body>
