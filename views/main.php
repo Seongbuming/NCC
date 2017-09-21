fd81982905f21e9acc35cd7eea23d2eec8315bef
SeongbumSeo <sBum.Seo@gmail.com>
2017-09-21 23:43:22 +0900

    메인 페이지

---
+++ views/main.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="ko">
+<head>
+    <meta charset="utf-8" />
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible"/>
+
+    <title>국가암정보센터</title>
+</head>
+
+<body>
+    <header>
+    </header>
+
+    <nav>
+    </nav>
+
+    <section id="contents">
+    </section>
+
+    <footer>
+    </footer>
+</body>
+</html>
