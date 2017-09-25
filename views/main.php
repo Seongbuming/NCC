0d5f289e76b7fd0325bfaae21b8c200b6c208fba
SeongbumSeo <sBum.Seo@gmail.com>
2017-09-25 12:55:56 +0900

    footer

--- views/main.php
+++ views/main.php
@@ -129,6 +129,39 @@
     </div>
 
     <div class="footer">
+        <div class="footer-top">
+            <div class="container">
+                <ul class="menu">
+                    <li><a href=".">개인정보처리방침</a></li>
+                    <li><a href=".">저작권 안내</a></li>
+                    <li><a href=".">이용안내</a></li>
+                    <li><a href=".">센터소개</a></li>
+                    <li><a href=".">사이트맵</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="container">
+                <img class="logo" src="images/logo_gray.png" alt="국가암정보센터 로고" />
+                <div class="copyright-pc">
+                    <p>경기도 고양시 일산동구 일산로 323 국립암센터 10408</p>
+                    <p>상담전화 : 1577 - 8899 / 홈페이지 운영관리 문의사항 : 031-920-2023 ( info@cancer.go.kr )</p>
+                    <p class="copyright">Copyright 2017 National Cancer Information Center. All rights reserved.</p>
+                    
+                    <div class="about">
+                        <p>※ 본 홈페이지는 보건복지부의 국민건강증진기금으로 제작되었으며, 광고 또는 상업적 콘텐츠를 포함하지 않고 있습니다.</p>
+                        <p>※ 본 정보는 진료를 대신할 수 없고, 법적으로 활용할 수 없습니다.</p>
+                    </div>
+                </div>
+                <div class="copyright-mobile">
+                    <p>경기도 고양시 일산동구 일산로 323 국립암센터 10408</p>
+                    <p>TEL : 1577 - 8899 (평일 09:00~18:00)</p>
+                    <p>E-mail : info@cancer.go.kr</p>
+                    <p class="copyright">Copyright 2017 국가암정보센터 All rights reserved.</p>
+                </div>
+                <img class="marks" src="images/marks.png" alt="마크" />
+            </div>
+        </div>
     </div>
     
     <script src="javascripts/jquery-1.12.4.min.js"></script>
