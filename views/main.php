17eca3b3d61e20047b51bc05ab427d13b33b6bee
SeongbumSeo <sBum.Seo@gmail.com>
2017-09-26 00:24:13 +0900

    CSS 전체 수정

--- views/main.php
+++ views/main.php
@@ -53,77 +53,115 @@
 
     <div class="contents">
         <div class="area area-search">
-            <h2>내가 알고 싶은 암 찾아보기</h2>
-            <input class="input-search" id="input-search-contents" type="text" title="검색 입력" />
-            <label class="label-search" for="input-search-contents">검색어를 입력해주세요</label>
-            <button class="button-search">검색</button>
-            <button class="view-all">전체암보기</button>
+            <div class="inside">
+                <h2>내가 알고 싶은 암 찾아보기</h2>
+                <input class="input-search" id="input-search-contents" type="text" title="검색 입력" />
+                <label class="label-search" for="input-search-contents">검색어를 입력해주세요</label>
+                <button class="button-search">검색</button>
+                <button class="view-all">전체암보기</button>
+            </div>
         </div>
 
         <div class="container-left">
             <div class="area">
-                <h2><a href=".">발암요인</a></h2>
-                <p>암예방을 위한 인체 발암요인정보를 알아볼 수 있습니다.</p>
+                <div class="inside">
+                    <h2><a href=".">발암요인</a></h2>
+                    <p>암예방을 위한 인체 발암요인정보를 알아볼 수 있습니다.</p>
+                </div>
             </div>
             <div class="area area-custom" style="background-image: url('images/hospicecare.png'); background-position: 90% 30%;">
-                <div class="center">    
-                    <p>아름다운 만남 행복한 동행</p>
-                    <h2 style="font-size: 1.7em;"><a href=".">호스피스완화의료</a></h2>
+                <div class="inside">
+                    <div class="center">
+                        <p>아름다운 만남 행복한 동행</p>
+                        <h2 style="font-size: 1.7em;"><a href=".">호스피스완화의료</a></h2>
+                    </div>
                 </div>
             </div>
 
             <div class="area">
-                <h2><a href=".">7대암검진권고</a></h2>
-                <p>사망률이 높은 암에 대한 검진 권고안을 새로이 개발한 것입니다.</p>
+                <div class="inside">
+                    <h2><a href=".">7대암검진권고</a></h2>
+                    <p>사망률이 높은 암에 대한 검진 권고안을 새로이 개발한 것입니다.</p>
+                </div>
             </div>
             <div class="area area-custom">
-                <div class="center">
-                    <img src="images/cancerprevention.png" alt="암 예방으로 삶 건강하게" style="vertical-align: middle;" />
-                    <h2 style="display: inline-block; vertical-align: middle;"><a href=".">암예방 10대 수칙</a></h2>
+                <div class="inside">
+                    <div class="center">
+                        <img src="images/cancerprevention.png" alt="암 예방으로 삶 건강하게" style="vertical-align: middle;" />
+                        <h2 style="display: inline-block; vertical-align: middle;"><a href=".">암예방 10대 수칙</a></h2>
+                    </div>
                 </div>
             </div>
 
             <div class="area">
-                <h2><a href=".">국가암등록사업 연례보고서</a></h2>
-                <p>국가암등록사업의 연간 통계를 다운로드하여 보실 수 있습니다.</p>
+                <div class="inside">
+                    <h2><a href=".">국가암등록사업 연례보고서</a></h2>
+                    <p>국가암등록사업의 연간 통계를 다운로드하여 보실 수 있습니다.</p>
+                </div>
             </div>
             <div class="area">
-                <h2><a href=".">생존자</a></h2>
-                <p>환자와 가족의 고통을 경감시켜 삶의 질을 향상시키는 의료 서비스입니다.</p>
+                <div class="inside">
+                    <h2><a href=".">생존자</a></h2>
+                    <p>환자와 가족의 고통을 경감시켜 삶의 질을 향상시키는 의료 서비스입니다.</p>
+                </div>
             </div>
         </div>
 
         <div class="container-right">
             <div class="call">
                 <div class="area area-call">
-                    <h2>암정보 상담센터</h2>
-                    <div class="call-detail">
-                        <img class="phonenumber" src="images/phonenumber.png" alt="1577-8899" />
-                        <p>평일: 09:00 ~ 18:00</p>
-                        <p>(주말 및 공휴일 휴무)</p>
+                    <div class="inside">
+                        <h2>암정보 상담센터</h2>
+                        <div class="call-detail">
+                            <img class="phonenumber" src="images/phonenumber.png" alt="1577-8899" />
+                            <p>평일: 09:00 ~ 18:00</p>
+                            <p>(주말 및 공휴일 휴무)</p>
+                        </div>
                     </div>
                 </div>
                 <div class="area area-call-links">
-                    <ul class="links">
-                        <li><a href=".">전화상담예약 바로가기</a></li>
-                        <li class="border"></li>
-                        <li><a href=".">이메일상담 바로가기</a></li>
-                    </ul>
+                    <div class="inside">
+                        <ul class="links">
+                            <li><a href=".">전화상담예약 바로가기</a></li>
+                            <li class="border"></li>
+                            <li><a href=".">이메일상담 바로가기</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
 
             <div class="area area-subscribe">
-                <h2>암연구동향</h2>
-                <p>국립암센터가 제공하는 국내외 최신 암연구정보</p>
-                <button class="subscribe">구독신청</button>
+                <div class="inside">
+                    <h2>암연구동향</h2>
+                    <p>국립암센터가 제공하는 국내외 최신 암연구정보</p>
+                    <button class="subscribe">구독신청</button>
+                </div>
             </div>
 
             <div class="area area-sns">
-                <a class="facebook" href="."><img src="images/icons/facebook_off.png" alt="페이스북" /></a>
-                <a class="twitter" href="."><img src="images/icons/twitter_off.png" alt="트위터" /></a>
-                <a class="blog" href="."><img src="images/icons/blog_off.png" alt="블로그" /></a>
-                <a class="post" href="."><img src="images/icons/post_off.png" alt="포스트" /></a>
-                <a class="youtube" href="."><img src="images/icons/youtube_off.png" alt="유튜브" /></a>
+                <div class="inside">
+                    <a class="facebook" href="."><img src="images/icons/facebook_off.png" alt="페이스북" /></a>
+                    <a class="twitter" href="."><img src="images/icons/twitter_off.png" alt="트위터" /></a>
+                    <a class="blog" href="."><img src="images/icons/blog_off.png" alt="블로그" /></a>
+                    <a class="post" href="."><img src="images/icons/post_off.png" alt="포스트" /></a>
+                    <a class="youtube" href="."><img src="images/icons/youtube_off.png" alt="유튜브" /></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="area area-notice">
+            <div class="inside">
+                <h2>공지사항</h2>
+            </div>
+        </div>
+
+        <div class="area area-apply">
+            <div class="inside">
+            </div>
+        </div>
+
+        <div class="area area-banner">
+            <div class="inside">
             </div>
         </div>
     </div>
